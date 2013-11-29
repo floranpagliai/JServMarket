b7cebd97e33fdc3d8ac188e07ca1c4e2d53656fc
Floran Pagliai <floran.pagliai@gmail.com>
2013-11-29 18:43:24 +0100

    Implement connexion MySQL in JconnexionSQL class

--- src/com/jserveur/Jcommand.java
+++ src/com/jserveur/Jcommand.java
@@ -4,6 +4,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 
 /**
  * com.jserveur in Jserveur
@@ -21,9 +26,42 @@ public class Jcommand {
     }
 
     public void exec(String command) {
-                if (command.equalsIgnoreCase("login")) {
-                    out_.println("veuillez vous identifier");
-                    out_.flush();
-                }
+        if (command.equalsIgnoreCase("login")) {
+            out_.println("veuillez vous identifier");
+            out_.flush();
+            test();
+        }
+    }
+
+    public void test() {
+        try {
+        Statement state = JconnexionSQL.getInstance().createStatement();
+
+
+        //L'objet ResultSet contient le résultat de la requête SQL
+        ResultSet result = state.executeQuery("SELECT * FROM users");
+        //On récupère les MetaData
+        ResultSetMetaData resultMeta = result.getMetaData();
+
+        System.out.println("\n**********************************");
+        //On affiche le nom des colonnes
+        for (int i = 1 ; i <= resultMeta.getColumnCount() ; i++)
+            System.out.print("\t" + resultMeta.getColumnName(i).toUpperCase() + "\t *");
+
+        System.out.println("\n**********************************");
+
+        while (result.next()) {
+            for (int i = 1 ; i <= resultMeta.getColumnCount() ; i++)
+                System.out.print("\t" + result.getObject(i).toString() + "\t |");
+
+            System.out.println("\n---------------------------------");
+
+        }
+
+        result.close();
+        state.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
